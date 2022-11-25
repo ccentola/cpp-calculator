9ac0d0fd4c4bda2c9c8e376c72c4b2d0b918130d
Carl Centola <carl.centola@gmail.com>
2022-11-25 14:44:55 -0700

    created working app in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+int calculate(int firstNum, char op, int secondNum){
+
+    int result;
+
+    if(op == '+'){
+        result = firstNum + secondNum;
+    } else if (op == '-'){
+        result = firstNum - secondNum;
+    } else if (op == '*'){
+        result = firstNum * secondNum;
+    } else if (op == '/'){
+        result = firstNum / secondNum;
+    } else {
+        cout << "Invalid operator" << endl;
+    }
+
+    return result;
+    
+}
+
 int main(){
-    cout << "hello world!" << endl;
+
+    int firstNum;
+    int secondNum;
+    char op;
+
+
+    cout << "Enter a number: ";
+    cin >> firstNum;
+    
+    cout << "Enter an operator: ";
+    cin >> op;
+
+    cout << "Enter a number: ";
+    cin >> secondNum;
+
+    int result = calculate(firstNum, op, secondNum);
+    cout << result << endl;
     return 0;
 }
